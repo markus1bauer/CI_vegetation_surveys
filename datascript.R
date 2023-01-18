25bc9bc0679d82c1e408d3a47762d15de8f62000
Markus Bauer <markus1.bauer@tum.de>
2023-01-18 17:20:27 +0100

    Update

--- datascript.R
+++ datascript.R
@@ -1,1 +1,8 @@
 print("Have this script run whatever data cleaning you do")
+
+library(here)
+library(dplyr)
+library(readr)
+
+data <- read_csv(here("data-raw", "data.csv")) %>%
+  select(period, BA)
