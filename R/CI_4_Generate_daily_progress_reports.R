7f0d94ae0f910f1d716dd8169aa948697f129a4f
Markus Bauer <markus1.bauer@tum.de>
2023-01-23 18:18:03 +0100

    Update CI_4_Generate_daily_progress_reports.R

--- R/CI_4_Generate_daily_progress_reports.R
+++ R/CI_4_Generate_daily_progress_reports.R
@@ -2,12 +2,3 @@
 # paper as seen here
 
 library(dplyr)
-library(readxl)
-library(readr)
-library(stringr)
-library(here)
-library(janitor)
-library(lubridate)
-library(patchwork)
-library(tidyr)
-library(ggplot2)
