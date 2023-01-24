b11ccddd651d3e66bf0eb19da6876717b689bacd
Markus Bauer <markus1.bauer@tum.de>
2023-01-24 15:44:21 +0100

    Update test_CI_1.R

--- testthat/test_CI_1.R
+++ testthat/test_CI_1.R
@@ -1,9 +1,11 @@
 library(testthat)
+library(here)
 context("checks that CI_1_Generate_reports is working")
 
 test_that("printed value is correct", {
-  
-  expect_output(str(source("../CI_1_Generate_reports.R")), 
-                   "Have this script run whatever data cleaning you do")
+
+  expect_output(
+    str(source(here("R", "CI_1_Generate_reports.R"))),
+    "Have this script run whatever data cleaning you do")
 
 })
