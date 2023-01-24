098d20f4b9a2880d5c2a9e76087f14be73514f85
Markus Bauer <markus1.bauer@tum.de>
2023-01-24 16:00:03 +0100

    Update test_CI_1.R

--- testthat/test_CI_1.R
+++ testthat/test_CI_1.R
@@ -1,11 +1,27 @@
 library(testthat)
 library(here)
+
 context("checks that CI_1_Generate_reports is working")
 
 test_that("printed value is correct", {
 
   expect_output(
     str(source(here("R", "CI_1_Generate_reports.R"))),
-    "Have this script run whatever data cleaning you do")
+    "Have this script run whatever data cleaning you do"
+    )
+  })
+
+test_that("There are different total cover values", {
+
+  report_exists  <-  ifelse(
+    file.exists(
+      file.path(
+        here("outputs", "different_total_cover.csv")
+        )
+      ),
+    TRUE, FALSE
+    )
+
+  expect_false(report_exists)
 
 })
