3cc26251beacca0c4f12e788d51b98082d6ce0fb
Markus Bauer <markus1.bauer@tum.de>
2023-03-18 10:54:53 +0100

    Remove outputs

--- R/CI_1_Generate_reports.R
+++ R/CI_1_Generate_reports.R
@@ -203,7 +203,6 @@ if (count(data) > 0) {
 
   print("CSV file printed with differences >20 and <(-5)")
   readr::write_csv(data, here("tests", "testthat", "different_total_cover.csv"))
-  readr::write_csv(data, here("outputs", "different_total_cover.csv"))
 
 } else {
 
