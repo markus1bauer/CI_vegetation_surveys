ac4e719aaa29c28b22fbdd364cdb2d1f4f1fc111
Markus Bauer <markus1.bauer@tum.de>
2023-03-20 11:13:04 +0100

    Update CI_2_Generate_warnings.R

--- R/CI_2_Generate_warnings.R
+++ R/CI_2_Generate_warnings.R
@@ -39,15 +39,6 @@ warning_messages <- c(
 
 # check if files exist and generate a plot with the warning ####
 
-if(exists("will_auto_fix_error_file"))
-  all_will_be_fixed <- paste(
-    c(
-      "ERRORS THAT WILL AUTO FIX:\n",
-      warning_messages[unique(will_auto_fix_error_file$error_name)]
-      ),
-    collapse = "\n"
-    ) else  all_will_be_fixed <- ""
-
 
 if(exists("warning_file"))
   all_warns <- paste(
@@ -58,17 +49,3 @@ if(exists("warning_file"))
       ),
     collapse = "\n"
     ) else all_warns = "No WARNINGS"
-
-
-filename <- file.path(here("testthat", "warnings.png"))
-
-
-if(length(all_warns) == 0 & length(all_will_be_fixed) == 0)  file.remove(filename)
-
-png(filename, width = 6, height = 0.7 + (0.2 * length(c(gregexpr("\\n", all_warns)[[1]], gregexpr("\\n", all_will_be_fixed)[[1]]))), units = "in", res = 300)
-par(mar = c(0,0,0,0))
-plot(0,0, axes = F, xlab = "", ylab = "", type = "n")
-text(0,0.7, all_warns, col = "red", cex = 0.6)
-text(0,0, all_will_be_fixed, col = "red", cex = 0.6, pos = 1)
-# title("warnings!!!", col.main= "red", xpd = NULL, line = -1)
-dev.off()
