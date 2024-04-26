eb580946be257ff8c844660d4a7866f60d02b544
markus1bauer <markus1.bauer@tum.de>
2024-04-27 01:23:00 +0200

    Update CI_1_Generate_reports.R

--- R/CI_1_Generate_reports.R
+++ R/CI_1_Generate_reports.R
@@ -19,7 +19,7 @@ library(stringr)
 library(tibble)
 library(tidyr)
 #library(webshot)
-#library(webshot2)
+library(webshot2)
 #webshot::install_phantomjs(force = TRUE)
 
 
@@ -149,7 +149,6 @@ if(count(data) == 0) {
     #gtsave(file)
 
 }
-
 
 
 write_csv(
